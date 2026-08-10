11364d0cac90a3d9711878b2b3158c641e1f9d6f
Isiah T Howard <139932709+ihoward40@users.noreply.github.com>
2026-08-10 15:05:12 -0400

    feat(amr): export runtime installer

--- apps/sintraprime/src/mission-runtime/index.ts
+++ apps/sintraprime/src/mission-runtime/index.ts
@@ -3,5 +3,6 @@ export * from './authorityEngine.js';
 export * from './budgetGovernor.js';
 export * from './modelRouter.js';
 export * from './missionRuntime.js';
+export * from './install.js';
 export * from './adapters/openAICompatibleModelAdapter.js';
 export * from './adapters/browserL0Tool.js';
